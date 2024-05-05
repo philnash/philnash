d1e8bb1b9d3294a0a724e18937bbf138e1366896
Phil Nash <philnash@gmail.com>
2024-05-05 14:34:21 +1000

    Updated dependencies and job

--- src/card.js
+++ src/card.js
@@ -19,38 +19,38 @@ const handle = "philnash";
 
 const data = [
   chalk.white.bold("          Work: ") +
-    chalk.gray("Developer Advocate for Sonar"),
+  chalk.gray("Developer Relations Engineer for ") + chalk.white("DataStax"),
   chalk.white.bold("     Home page: ") + chalk.blue("https://philna.sh"),
   "",
   chalk.white.bold("       Twitter: ") +
-    chalk.grey("https://twitter.com/") +
-    chalk.cyan(handle),
+  chalk.grey("https://twitter.com/") +
+  chalk.cyan(handle),
   chalk.white.bold("      Mastodon: ") +
-    chalk.grey("https://mastodon.social/") +
-    chalk.magenta(`@${handle}`),
+  chalk.grey("https://mastodon.social/") +
+  chalk.magenta(`@${handle}`),
   chalk.white.bold("      LinkedIn: ") +
-    chalk.grey("https://www.linkedin.com/in/") +
-    chalk.blue(handle),
+  chalk.grey("https://www.linkedin.com/in/") +
+  chalk.blue(handle),
   "",
   chalk.white.bold("           DEV: ") +
-    chalk.grey("https://dev.to/") +
-    chalk.white(handle),
+  chalk.grey("https://dev.to/") +
+  chalk.white(handle),
   chalk.white.bold("        Medium: ") +
-    chalk.grey("https://medium.com/") +
-    chalk.white(`@${handle}`),
+  chalk.grey("https://medium.com/") +
+  chalk.white(`@${handle}`),
   "",
   chalk.white.bold("        GitHub: ") +
-    chalk.grey("https://github.com/") +
-    chalk.white(handle),
+  chalk.grey("https://github.com/") +
+  chalk.white(handle),
   chalk.white.bold("      RubyGems: ") +
-    chalk.grey("https://rubygems.org/profiles/") +
-    chalk.red(handle),
+  chalk.grey("https://rubygems.org/profiles/") +
+  chalk.red(handle),
   chalk.white.bold("           npm: ") +
-    chalk.grey("https://www.npmjs.com/") +
-    chalk.magenta(`~${handle}`),
+  chalk.grey("https://www.npmjs.com/") +
+  chalk.magenta(`~${handle}`),
   chalk.white.bold("Stack Overflow: ") +
-    chalk.grey("https://stackoverflow.com/users/28376/") +
-    chalk.yellow(handle),
+  chalk.grey("https://stackoverflow.com/users/28376/") +
+  chalk.yellow(handle),
 ];
 
 const output = boxen(data.join("\n"), boxOptions);
